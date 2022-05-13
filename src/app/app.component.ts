6e8949da3316a4d3b5fa2280adc56fa17c5e429b
Jamal <jamal72e@gmail.com>
2022-05-13 09:58:44 +0430

    Update Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstProject';
+password:string='';
+includeLatters:boolean=false;
+includeNums:boolean=false;
+includeSymbols:boolean=false;
+length : number =0;
+
+constructor(){
+
+}
+  onButtonClick(){
+    const numbers='0123456789';
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const symbols='!@#$%^&*()_+';
+    let validChar='';
+    if (this.includeNums) {
+      validChar+=numbers;
+    }
+    if (this.includeLatters) {
+      validChar+=letters;
+    }
+    if (this.includeSymbols) {
+      validChar+=symbols;
+    }
+    let generatedPassword='';
+    for(let index=0;index<this.length;index++){
+      let index=Math.floor(Math.random()*validChar.length);
+      generatedPassword+=validChar[index];
+    }
+
+    this.password=generatedPassword;
+
+  }
+  getPassword(){
+   return this.password;
+  }
+
+  onChangeLatter(){
+    this.includeLatters=!this.includeLatters;
+  }
+
+  onChangeNumbers(){
+    this.includeNums=!this.includeNums;
+  }
+
+  onChangeSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+
+  onChangeLength(value:string){
+    //console.log(event);
+    const parsedValue=parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length=parsedValue;
+    }
+
+  }
+
 }
